b546019ca3db7eace0bffb18149d0b419ad535c6
Nour Afram <nouara.afram@hotmail.com>
2023-11-06 15:52:47 +0100

    AntalTimmar

---
+++ src/inlämningsuppgift/ReadWriteText.java
@@ -0,0 +1,32 @@
+package inlämningsuppgift;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Scanner;
+public class ReadWriteText {
+
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in); // scanna in det som användaren skriver in
+        System.out.println("Var vänlig och berätta om problemet som uppstår med bredband uppkopplingen!, skriv stop för att avsluta ärendet.");
+        // ett meddelande till användaren
+
+        String input = ""; // vi deklarerar denna variabel för att lagra texten som användaren skriver
+
+        while (true) { // vi kör en while loop som körs tills användaren skriver ordet Stop eller stop
+            String line = scan.nextLine();
+
+            if (line.equals("stop") || line.equals("Stop")) {
+                break;
+            }
+
+            input += line + "\n";
+
+        }
+
+
+    }
+
+    }
