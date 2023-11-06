f0cbac22b16a9e8abc54043fe9f8849a4a7877cf
Nour Afram <nouara.afram@hotmail.com>
2023-11-06 15:57:36 +0100

    AntalTimmar

---
+++ src/JämnaTalFrånEttTillHundra.java
@@ -0,0 +1,16 @@
+public class JämnaTalFrånEttTillHundra {
+
+    public static void main(String[] args) {
+
+        int even = 2;
+
+        System.out.println("Even numbers from 1 - 100 are:");
+
+        while (even <= 100) {
+
+            System.out.println(even);
+
+            even = even + 2; // kan skrivas som even += 2;
+        }
+    }
+}
